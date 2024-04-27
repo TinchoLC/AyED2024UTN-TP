a80f692b4a007ed6366336246969526a589bfaf2
TinchoLC <104269861+TinchoLC@users.noreply.github.com>
2024-04-27 15:29:39 -0300

    Creado archivo del main y avance en el menu principal
    Descripción

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # AyED2024UTN-TP
 Repositorio para el TP de ALGORITMOS Y ESTRUCTURAS DE DATOS 2024, de la materia Ingeniería en Sistemas de Información de la UTN
 
-Cabrera, Martín
-Delgado, Mauro
-Rodriguez, Lautaro
-Rossi, Dariana
+Integrantes del grupo:
+- Cabrera, Martín
+- Delgado, Mauro
+- Rodriguez, Lautaro
+- Rossi Avonto, Dariana
 
 Entrega primer TP: https://frro.cvg.utn.edu.ar/mod/assign/view.php?id=65227
---
+++ main.py
@@ -0,0 +1,21 @@
+# Archivo principal
+
+print("1. Gestionar mi perfil")
+print("2. Gestionar candidatos")
+print("3. Matcheos")
+print("4. Reportes estadisticos")
+print("0. Salir \n")
+
+opcion = int(input("Seleccione la opcion: "))
+
+while(opcion != 0):
+	if(opcion == 1):
+		print("Elegiste gestionar tu perfil")
+
+
+	if(opcion>4 or opcion<0):
+		opcion = int(input("Opcion incorrecta, seleccione otra opcion: "))
+	else:
+		opcion = int(input("\nSeleccione otra opcion: "))
+
+print("Programa terminado.")
